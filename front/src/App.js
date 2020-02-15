405e2c880576c52bb9fe4b0057799c1c19fb6e35
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-02-15 16:01:33 -0300

    Adicionando estrutura do layout

--- front/src/App.js
+++ front/src/App.js
@@ -2,10 +2,35 @@ import React from 'react';
 import api from './services/api';
 
 import './App.css';
+import './global.css';
+import './Main.css';
+import './Header.css';
 
 function App() {
+  
   return (
-    <h1>Hello World!</h1>
+    <div id="app">
+      <header>
+        <strong>GQUIZ</strong>
+      </header>
+
+      <main>
+        <div id="programs">
+          <ul>
+            <li>ENCONTRO COM FÁTIMA BERNARDES</li>
+            <li>BBB 20</li>
+            <li>DOMINGÃO DO FAUSTÃO</li>
+            <li>É DE CASA</li>
+          </ul>
+        </div>
+      </main>
+
+      <aside>
+        <div id="score">
+
+        </div>
+      </aside>
+    </div>
   );
 }
 

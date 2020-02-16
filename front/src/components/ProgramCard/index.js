3a3f1dcecc88fb334af17d7764291773f7bea613
Henry jr <henryjulianrosajunior@gmail.com>
2020-02-16 16:44:52 -0300

    adicioando poput de leitura de questionario

--- front/src/components/ProgramCard/index.js
+++ front/src/components/ProgramCard/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Image, Info, Title, Description } from './styles';
+import { Card, InputGroup, Modal, Button, FormControl } from 'react-bootstrap';
 
 export default function Header(questionary) {
+    //Popup p/ cadastrar questionario
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
   return (
-    < Container className="col-sm-4">
+    < Container className="col-sm-4" >
       <Info>
-        <Title className="">{questionary.formName}</Title>
+        <Title className="" onClick={handleShow}>{questionary.formName}</Title>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Questionário</Modal.Title>
+          </Modal.Header>
+          <Modal.Body >
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="light" onClick={handleClose}>
+              Cancelar
+        </Button>
+            <Button variant="dark" onClick={handleClose}>
+              Salvar
+        </Button>
+          </Modal.Footer>
+        </Modal>
       </Info>
     </Container>
   );

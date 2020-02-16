ce3d54438fc00e85ebd02418d9aaf52d80748a11
Henry jr <henryjulianrosajunior@gmail.com>
2020-02-16 17:41:11 -0300

    commit campos questionário

--- front/src/components/ProgramCard/index.js
+++ front/src/components/ProgramCard/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Container, Image, Info, Title, Description } from './styles';
 import { Card, InputGroup, Modal, Button, FormControl } from 'react-bootstrap';
 import QuestionModal from '../../components/QuestionModal';
+import Item from '../QuestionInpunt'
 
 export default function Header(questionary) {
     //Popup p/ cadastrar questionario
@@ -9,19 +10,24 @@ export default function Header(questionary) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const questions = questionary.questions;
   return (
     < Container className="col-sm-4" >
       <Info>
         <Title className="" onClick={handleShow}>{questionary.formName}</Title>
+
         <QuestionModal
           questionary={questionary}
         />
+
         <Modal show={show} onHide={handleClose} animation={false}>
           <Modal.Header closeButton>
             <Modal.Title>Questionário</Modal.Title>
           </Modal.Header>
           <Modal.Body >
-            
+            {questions.map((question) => (
+             <Item {...question}/>
+            ))}
           </Modal.Body>
           <Modal.Footer>
             <Button variant="light" onClick={handleClose}>

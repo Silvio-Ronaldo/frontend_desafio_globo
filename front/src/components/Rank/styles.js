8def16f14a7357fbcd003962006ad56f5fa52b7b
Joed Silva <joedsilva2016@gmail.com>
2020-02-16 12:23:20 -0300

    Adicionando rank

---
+++ front/src/components/Rank/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content:space-between;
+`;
+
+export const Name = styled.p``;
+
+export const Score = styled.p``;
--- front/src/pages/Main/index.js
+++ front/src/pages/Main/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Card, InputGroup, Modal, Button, FormControl } from 'react-bootstrap';
 import { MdAddCircleOutline } from 'react-icons/md';
 import Header from '../../components/Header';
+import Rank from '../../components/Rank';
 import Item from '../../components/Card';
 import { Container } from './styles.js';
 import api from '../../services/api.js';
@@ -39,47 +40,47 @@ function Main() {
               <Card.Header as="h5">
                 Programação
                 <Button variant="light" as="h5" className="float-right" onClick={handleShow}>
-                <MdAddCircleOutline size={25} color="black"/>
-              </Button>              
-              <Modal show={show} onHide={handleClose} animation={false}>
-              <Modal.Header closeButton>
-                <Modal.Title>Cadastrar Programas</Modal.Title>
-              </Modal.Header>
-              <Modal.Body >
-                <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text  id="inputGroup-sizing-default">Nome</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-                </InputGroup>             
-                <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="inputGroup-sizing-default">Horário</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-                </InputGroup>                
-                <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>Descrição</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl as="textarea" aria-label="With textarea" />
-                </InputGroup>
-              </Modal.Body>              
-              <Modal.Footer>
-                <Button variant="light" onClick={handleClose} type="submit">
-                  Cancelar
+                  <MdAddCircleOutline size={25} color="black" />
                 </Button>
-                <Button variant="dark" onClick={handleClose}>
-                  Salvar
+                <Modal show={show} onHide={handleClose} animation={false}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Cadastrar Programas</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body >
+                    <InputGroup className="mb-3">
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-default">Nome</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                      />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-default">Horário</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                      />
+                    </InputGroup>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text>Descrição</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl as="textarea" aria-label="With textarea" />
+                    </InputGroup>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="light" onClick={handleClose} type="submit">
+                      Cancelar
                 </Button>
-              </Modal.Footer>
-              </Modal>
+                    <Button variant="dark" onClick={handleClose}>
+                      Salvar
+                </Button>
+                  </Modal.Footer>
+                </Modal>
               </Card.Header>
               <Card.Body className="overflow-auto">
                 <Card.Text className="row justify-content-center" id="program-container">
@@ -93,13 +94,8 @@ function Main() {
               <Card.Header as="h5">Rank de Usuários</Card.Header>
               <Card.Body className="overflow-auto">
                 <Card.Text>
-                  {users.map(user => (
-                    <Card.Body>
-                      <Card.Title className="row">
-                        <a className="col-sm-8">{user.name}</a>
-                        <a className="col-sm-4">Score:{user.score}</a>
-                      </Card.Title>
-                    </Card.Body>
+                  {users.map((user) => (
+                    < Rank {...user} />
                   ))}
                 </Card.Text>
               </Card.Body>

e1b8f7f3119204e688717947ae5fb38c2e9173fb
Joed Silva <joedsilva2016@gmail.com>
2020-02-16 00:19:39 -0300

    Adicionando axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import api from './services/api';
 import './App.css';
-import {Navbar, Nav, Form, FormControl, Button, Card} from 'react-bootstrap';
-import {MdAddCircleOutline} from 'react-icons/md';
+import { Navbar, Nav, Form, FormControl, Button, Card } from 'react-bootstrap';
+import { MdAddCircleOutline } from 'react-icons/md';
 
 function App() {
-  
-  return ( 
+  const [programs, setPrograms] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadInfo() {
+      const programs = await api.get('/listPrograms');
+      setPrograms(programs.data);
+
+      const users = await api.get('/getUsers');
+      setUsers(users.data);
+    }
+
+    loadInfo();
+  }, []);
+
+  return (
     <div className="h-100">
       <Navbar bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="#home">Globo Quiz</Navbar.Brand>
@@ -24,32 +38,38 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      
-        <div className="container-fluid h-100">
-          <div className="row h-100">
+
+      <div className="container-fluid h-100">
+        <div className="row h-100">
           <Card className="col-sm-8 div-main-body">
             <Card.Header as="h5">
               Programação
-              <MdAddCircleOutline className="float-right" size={25} color="black" Link/>
+              <MdAddCircleOutline className="float-right" size={25} color="black" Link />
             </Card.Header>
-              <Card.Body className="overflow-auto"> 
-                <Card.Text>
-              
+            <Card.Body className="overflow-auto">
+              {programs.map(program => (
+                <Card.Text className="row" id="program-container">
+                  {program.name}
                 </Card.Text>
-              </Card.Body>
-            </Card>
+              ))}
+            </Card.Body>
+          </Card>
 
-            <Card className="col-sm-4 div-main-body">
+          <Card className="col-sm-4 div-main-body">
             <Card.Header as="h5">Rank de Usuários</Card.Header>
-              <Card.Body className="overflow-auto"> 
-                <Card.Text>
-              
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
+            <Card.Body className="overflow-auto">
+              <Card.Text>
+                {users.map(user => (
+                  <Card.Body>
+                    <Card.Title>{user.name}</Card.Title>
+                  </Card.Body>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
       </div>
+    </div >
   );
 }
 
--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://https://api.github.com',
+    baseURL: 'https://globoapi.herokuapp.com',
 });
 
 export default api;
